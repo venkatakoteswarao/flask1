4d872f960e55ddff0f46a5cd3ba320573d97b697
venkatakoteswarao <158768864+venkatakoteswarao@users.noreply.github.com>
2024-12-22 11:25:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form.get('user_input')
+    return f"Received: {data}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
